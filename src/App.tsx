47e9ca03a6c4de0997dbf75b836c4fc137f9f805
AzrilYusuf <muhammadazrilyusuf.w3rk@gmail.com>
2024-10-08 17:06:48 +0700

    init(project): Initialise project and orginise file system

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+// import { useEffect } from 'react'
+// import { useLocation } from 'react-router-dom';
+import "./styles/App.css";
+
+const App = () => {
+  // const location = useLocation();
+
+  // useEffect(() => {
+  //   switch (location.pathname) {
+  //     case '/':
+  //       document.title = 'Home Page';
+  //       break;
+  //     case '/login':
+  //       document.title = 'Login';
+  //       break;
+  //     case '/register':
+  //       document.title = 'Register';
+  //       break;
+  //     default:
+  //       document.title = 'Videobelajar Course';
+  //       break;
+  //   }
+  // }, [location.pathname]);
+
+  return (
+  <></>
+);
+};
+
+export default App;
