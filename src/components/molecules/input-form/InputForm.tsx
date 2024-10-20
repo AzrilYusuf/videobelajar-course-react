0a31d4c7975dc2a8e2ab65fed265db0e30db227f
AzrilYusuf <muhammadazrilyusuf.w3rk@gmail.com>
2024-10-21 01:58:15 +0700

    feat(toggle-password): Add function onClick in toggle password button in order to hide/show password.

--- src/components/molecules/input-form/InputForm.tsx
+++ src/components/molecules/input-form/InputForm.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Label from "../../atoms/Label";
 import Input from "../../atoms/Input";
 import Image from "../../atoms/Image";
 import { FormTypes } from "../../../interfaces/interfaces";
+import ToggleShow from "../../../assets/svg/toggle-show.svg";
 import ToggleHide from "../../../assets/svg/toggle-hide.svg";
 
 const InputForm: React.FC<FormTypes> = ({
@@ -15,40 +16,56 @@ const InputForm: React.FC<FormTypes> = ({
   toggleHide,
   handleChange,
 }) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const handleTogglePassword = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <Label htmlFor={htmlFor} value={labelValue} />
-      <div style={{display: "flex", alignItems: "center"}}>
-      {isSelect === true ? (
-        <div className="pnumber-select" style={{ width: "100%" }}>
-          <select id={id} name="phone-number">
-            <option value="1">+62</option>
-            <option value="2">+63</option>
-            <option value="3">+64</option>
-            <option value="4">+65</option>
-          </select>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        {isSelect === true ? (
+          <div className="pnumber-select" style={{ width: "100%" }}>
+            <select id={id} name="phone-number">
+              <option value="1">+62</option>
+              <option value="2">+63</option>
+              <option value="3">+64</option>
+              <option value="4">+65</option>
+            </select>
+            <Input
+              id={id}
+              type={inputType}
+              name={inputName}
+              style={{ width: "100%" }}
+              handleChange={handleChange}
+            />
+          </div>
+        ) : (
           <Input
             id={id}
-            type={inputType}
+            type={
+              isPasswordVisible && inputType === "password" ? "text" : inputType
+            }
             name={inputName}
-            style={{ width: "100%" }}
+            style={{ flex: "1" }}
             handleChange={handleChange}
           />
-        </div>
-      ) : (
-        <Input
-          id={id}
-          type={inputType}
-          name={inputName}
-          style={{ flex: "1" }}
-          handleChange={handleChange}
-        />
-      )}
-      {toggleHide === true && (
-        <button type="button" id="togglePassword" style={{ marginLeft: "8px" }}>
-          <Image src={ToggleHide} alt="Toggle-hide-password" />
-        </button>
-      )}
+        )}
+        {toggleHide === true && (
+          <button
+            type="button"
+            id="togglePassword"
+            style={{ marginLeft: "8px" }}
+            onClick={handleTogglePassword}
+          >
+            <Image
+              src={isPasswordVisible ? ToggleHide : ToggleShow}
+              alt={isPasswordVisible ? "Hide password" : "Show password"}
+            />
+          </button>
+        )}
       </div>
     </div>
   );
